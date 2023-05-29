3aa68b6668f5774b6074bf5f8220ecdd6a77f32c
ale2580 <121447069+aalllee@users.noreply.github.com>
2023-05-29 16:23:15 +0300

    Item Effects

--- Public/items/Item.h
+++ Public/items/Item.h
@@ -60,6 +60,10 @@ protected:
 	UPROPERTY(VisibleAnywhere)
 	USphereComponent* Sphere;
 
+	UPROPERTY(EditAnywhere)
+	class UNiagaraComponent* EmbersEffect;
+	
+
 private:
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
